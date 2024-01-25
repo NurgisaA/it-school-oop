fb27f0ef6650cd0cc0222c5a69865b404197933e
nurgisa <apbaz.nurgisa@gmail.com>
2024-01-25 19:00:39 +0500

    add twig

--- src/example8/TaskModel.php
+++ src/example8/TaskModel.php
@@ -5,17 +5,31 @@ namespace Itschool\Lesson4\Example8;
 class TaskModel {
     private array $tasks = [];
 
+    public function __construct()
+    {
+        $this->reloadDB();
+    }
+
+
     public function addTask(string $task): void {
+        $this->reloadDB();
+
         $this->tasks[] = $task;
         $json = json_encode($this->tasks);
+
         file_put_contents("db.json", $json);
 
     }
 
     public function getTasks(): array {
-        $json = file_get_contents("db.json");
-        $json = json_decode($json);
-        $this->tasks = array_replace($this->tasks, $json);
+        $this->reloadDB();
         return $this->tasks;
+    }
+
+    private function reloadDB(): void
+    {
+        $db = file_get_contents("db.json");
+        $json = json_decode($db);
+        $this->tasks = array_replace($this->tasks, $json);
     }
 }
